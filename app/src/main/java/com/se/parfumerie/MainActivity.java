14cdadc1513314c65dab735722133df8666b0720
Brasneni Mario <mariobrasneni@gmail.com>
2023-03-22 14:53:07 +0200

    displaying questions

--- app/src/main/java/com/se/parfumerie/MainActivity.java
+++ app/src/main/java/com/se/parfumerie/MainActivity.java
@@ -1,12 +1,16 @@
 package com.se.parfumerie;
 
-import androidx.appcompat.app.AppCompatActivity;
+import static android.content.ContentValues.TAG;
 
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
-import com.google.android.gms.tasks.Task;
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
@@ -14,12 +18,16 @@ import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 
 import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
-
-    private TextView tw;
+    private List<String> questions;
+    private TextView textView;
+    private Button skipButton;
+    private int currentQuestionIndex;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,18 +35,38 @@ public class MainActivity extends AppCompatActivity {
         getSupportActionBar().hide();
         setContentView(R.layout.activity_main);
 
-        tw = findViewById(R.id.question);
+        textView = findViewById(R.id.question);
+        skipButton = findViewById(R.id.skip);
+        questions = new ArrayList<>();
 
         FirebaseDatabase database = FirebaseDatabase.getInstance();
         DatabaseReference myRef = database.getReference("Intrebari");
-        List<String> intrebari = new ArrayList<>();
 
-        Task<DataSnapshot> dataSnapshot = myRef.get();
-        for (DataSnapshot snapshot : dataSnapshot.) {
-            intrebari = snapshot.getValue(String.class);
+        myRef.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                for (DataSnapshot data : snapshot.getChildren()) {
+                    questions.add(data.getValue(String.class));
+                }
+                textView.setText(questions.get(0));
+                Collections.shuffle(questions);
+            }
 
-        }
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+                Log.e(TAG, "Error retrieving questions from Firebase", error.toException());
+            }
+        });
+
+        skipButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(currentQuestionIndex + 1 < questions.size()) {
+                    currentQuestionIndex++;
+                }
+                textView.setText(questions.get(currentQuestionIndex));
+            }
+        });
+
     }
-
-
 }
