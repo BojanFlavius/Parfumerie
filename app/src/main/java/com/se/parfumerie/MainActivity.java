4959f34d89839273f426d5b08bfcfbdd9b8b36b8
Flavius <flavibojan@gmail.com>
2023-03-22 11:48:19 +0200

    database

--- app/src/main/java/com/se/parfumerie/MainActivity.java
+++ app/src/main/java/com/se/parfumerie/MainActivity.java
@@ -3,13 +3,42 @@ package com.se.parfumerie;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.TextView;
+
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+
+
+    private TextView tw;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         getSupportActionBar().hide();
         setContentView(R.layout.activity_main);
+
+        tw = findViewById(R.id.question);
+
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+        DatabaseReference myRef = database.getReference("Intrebari");
+        List<String> intrebari = new ArrayList<>();
+
+        Task<DataSnapshot> dataSnapshot = myRef.get();
+        for (DataSnapshot snapshot : dataSnapshot.) {
+            intrebari = snapshot.getValue(String.class);
+
+        }
     }
+
+
 }
